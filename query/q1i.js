364d583e5f17d0a2b7aaff399436c5a2ff924086
hoshino_cola <2835327895@qq.com>
2024-12-15 18:37:57 +0800

    update: finished task1

--- query/q1i.js
+++ query/q1i.js
@@ -1,5 +1,24 @@
 // Task 1i
 
-db.todo.aggregate([
-    // TODO: Write your query here
+db.keywords.aggregate([
+    {
+        $match: {
+            keywords: {
+                $elemMatch: {
+                    name: {$in: ["mickey mouse", "marvel comic"]}
+                }
+            }
+        }
+    },
+    {
+        $project: {
+            movieId: 1,
+            _id: 0
+        }
+    },
+    {
+        $sort: {
+            movieId: 1
+        }
+    }
 ]);
--- query/q1iii.js
+++ query/q1iii.js
@@ -1,5 +1,24 @@
 // Task 1iii
 
-db.todo.aggregate([
+db.ratings.aggregate([
     // TODO: Write your query here
+    {
+        $group: {
+            _id: "$rating",
+            count: {$sum: 1},
+        }
+    },
+    {
+        $sort: {
+            // _id: "$rating",
+            _id: -1
+        }
+    },
+    {
+        $project: {
+            count: 1,
+            rating: "$_id",
+            _id: 0
+        }
+    }
 ]);
