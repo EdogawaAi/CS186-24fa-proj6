693d760de4d97a2876224c633d6d389e4640c42d
hoshino_cola <2835327895@qq.com>
2024-12-15 20:26:54 +0800

    update: finished proj6

--- query/q3i.js
+++ query/q3i.js
@@ -1,5 +1,38 @@
 // Task 3i
 
-db.todo.aggregate([
+db.credits.aggregate([
     // TODO: Write your query here
+    {
+        $unwind: "$cast"
+    },
+    {
+        
+        $match: {
+            "cast.id": 7624
+        }
+    },
+    {
+        $lookup: {
+            from: "movies_metadata", // collection to join
+            localField: "movieId", // field from credits
+            foreignField: "movieId", // field from movies_metadata
+            as: "movie_info" // output array
+        }
+    },
+    {
+        $unwind: "$movie_info"
+    },
+    {
+        $project: {
+            _id: 0,
+            title: "$movie_info.title",
+            release_date: "$movie_info.release_date",
+            character: "$cast.character"
+        }
+    },
+    {
+        $sort: {
+            release_date: -1
+        }
+    }
 ]);
--- query/q3ii.js
+++ query/q3ii.js
@@ -1,5 +1,44 @@
 // Task 3ii
 
-db.todo.aggregate([
+db.credits.aggregate([
     // TODO: Write your query here
+    {
+        $match: {
+            crew: {
+                $elemMatch: {
+                    id: 5655,
+                    job: "Director"
+                }
+            }
+        }
+    },
+    {
+        $unwind: "$cast"
+    },
+    {
+        $group: {
+            _id: {
+                id: "$cast.id",
+                name: "$cast.name"
+            },
+            count: {$sum: 1}
+        }
+    },
+    {
+        $project: {
+            _id: 0,
+            id: "$_id.id",
+            name: "$_id.name",
+            count: 1
+        }
+    },
+    {
+        $sort: {
+            count: -1,
+            id: 1
+        }
+    },
+    {
+        $limit: 5
+    }
 ]);
